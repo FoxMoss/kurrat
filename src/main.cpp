9bb5e9d624076d1a8fa8778ffa360d15fec4ac31
FoxMoss <foxmoss@mediaology.com>
2025-12-16 23:42:40 -0600

    wip: working on authenticate cell still have to do tls

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
-#include "mbedtls/pk.h"
-#include "psa/crypto.h"
 #include "tor.hpp"
 #include <arpa/inet.h>
-#include <bit>
-#include <concepts>
 #include <cstddef>
 #include <cstdint>
-#include <iomanip>
-#include <iostream>
 #include <linux/tls.h>
 #include <mbedtls/aes.h>
 #include <mbedtls/ctr_drbg.h>
+#include <mbedtls/ecdsa.h>
 #include <mbedtls/entropy.h>
 #include <mbedtls/gcm.h>
 #include <mbedtls/net_sockets.h>
@@ -18,7 +13,8 @@
 #include <mbedtls/ssl.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#include <optional>
+#include <sodium.h>
+#include <sodium/crypto_sign.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,6 +22,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <utility>
 #include <vector>
 
 void tls_key_export_callback(void *p_expkey, mbedtls_ssl_key_export_type type,
@@ -55,8 +52,59 @@ void tls_key_export_callback(void *p_expkey, mbedtls_ssl_key_export_type type,
   fprintf(keylog_file, "\n");
   fflush(keylog_file);
 }
-int main() {
 
+struct LinkKeys {
+  std::vector<uint8_t> link_public_key, link_secret_key, cert;
+};
+
+std::optional<LinkKeys> create_link_cert() {
+  uint8_t link_public_key[32];
+  uint8_t link_secret_key[64];
+
+  crypto_sign_keypair(link_public_key, link_secret_key);
+
+  FILE *signing_secret_key = fopen(
+      "/home/foxmoss/Projects/TorVPN/keys/ed25519_signing_secret_key", "rb");
+
+  fseek(signing_secret_key, 0x20, SEEK_SET);
+
+  uint8_t secret_key[64];
+  fread(secret_key, 1, 64, signing_secret_key);
+  fclose(signing_secret_key);
+
+  std::vector<uint8_t> cert;
+  cert.push_back(1); // version
+  cert.push_back(
+      0x06); // Ed25519 authentication key signed with ed25519 signing key
+
+  uint32_t expiration = 37313477; // this impl will stop working in 2030
+  expiration = htonl(expiration);
+  cert.insert(cert.end(), (uint8_t *)&expiration,
+              (uint8_t *)&expiration + sizeof(uint32_t));
+
+  cert.push_back(0x01); // certified ed25519
+  cert.insert(cert.end(), link_public_key, link_public_key + 32);
+
+  cert.push_back(0x00); // no extensions
+
+  unsigned char signature[crypto_sign_BYTES];
+  crypto_sign_detached(signature, NULL, cert.data(), cert.size(), secret_key);
+
+  cert.insert(cert.end(), signature, signature + crypto_sign_BYTES);
+
+  std::vector<uint8_t> link_public_key_v, link_secret_key_v;
+  link_public_key_v.insert(link_public_key_v.end(), link_public_key,
+                           link_public_key + 32);
+  link_secret_key_v.insert(link_secret_key_v.end(), link_secret_key,
+                           link_secret_key + 64);
+
+  LinkKeys keys{.link_public_key = link_public_key_v,
+                .link_secret_key = link_secret_key_v,
+                .cert = cert};
+  return keys;
+}
+
+int main() {
   mbedtls_net_context server_ctx;
   mbedtls_ssl_context ssl;
   mbedtls_ssl_config conf;
@@ -100,18 +148,71 @@ int main() {
   }
   mbedtls_net_set_nonblock(&server_ctx);
 
-  TorConnection connection;
+  // generate auth cert now
 
-  // https://torspec-12e191.pages.torproject.net/tor-spec/opening-streams.html#opening
-  std::string inital_addr = "107.189.1.175:9001";
+  if (sodium_init() == -1) {
+    printf("FATAL ERROR: Sodium init failed \n");
+    return -1;
+  }
 
-  std::vector<uint8_t> return_buffer = {};
-  connection.generate_versions_cell(return_buffer);
+  auto cert = create_link_cert();
+
+  // reduce size format of ip addresses
+  std::string other_addr_str = "107.189.1.175";
+
+  struct in_addr other_addr;
+  inet_pton(AF_INET, other_addr_str.c_str(), &other_addr);
+  uint32_t other_addr_raw = other_addr.s_addr;
+
+  std::string my_addr_str = "205.185.125.167";
+
+  struct in_addr my_addr;
+  inet_pton(AF_INET, my_addr_str.c_str(), &my_addr);
+  uint32_t my_addr_raw = my_addr.s_addr;
+
+  // read rsa keys
+  FILE *signing_secret_key_rsa =
+      fopen("/home/foxmoss/Projects/TorVPN/keys/secret_id_key", "rb");
+
+  fseek(signing_secret_key_rsa, 0, SEEK_END);
+  size_t signing_secret_key_rsa_len = ftell(signing_secret_key_rsa);
+  fseek(signing_secret_key_rsa, 0, SEEK_SET);
+
+  std::vector<uint8_t> secret_key_rsa;
+  secret_key_rsa.insert(secret_key_rsa.end(), signing_secret_key_rsa_len, 0);
+  fread(secret_key_rsa.data(), 1, signing_secret_key_rsa_len,
+        signing_secret_key_rsa);
+  fclose(signing_secret_key_rsa);
+
+  // read public id key
+
+  FILE *signing_public_key = fopen(
+      "/home/foxmoss/Projects/TorVPN/keys/ed25519_signing_public_key", "rb");
+
+  fseek(signing_public_key, 0x20, SEEK_SET);
+
+  std::vector<uint8_t> public_key;
+  public_key.insert(public_key.end(), 32, 0);
+  fread(public_key.data(), 1, 32, signing_public_key);
+  fclose(signing_public_key);
+
+  // start the tor connection
+
+  TorConnection connection(cert->cert, public_key, cert->link_secret_key,
+                           cert->link_public_key, my_addr_raw, other_addr_raw,
+                           secret_key_rsa);
+
+  std::vector<uint8_t> send_buffer = {};
+  std::vector<uint8_t> initiator_log = {};
+  connection.generate_versions_cell(send_buffer);
 
   FILE *from_me = fopen("from_me.log", "w");
-  mbedtls_ssl_write(&ssl, return_buffer.data(), return_buffer.size());
-  fwrite(return_buffer.data(), return_buffer.size(), 1, from_me);
+  mbedtls_ssl_write(&ssl, send_buffer.data(), send_buffer.size());
+  fwrite(send_buffer.data(), send_buffer.size(), 1, from_me);
+  initiator_log.insert(initiator_log.end(), send_buffer.begin(),
+                       send_buffer.end());
   fflush(from_me);
+  send_buffer.clear();
 
   std::vector<uint8_t> read_buffer;
   int size;
@@ -133,8 +234,17 @@ int main() {
     printf("read! %i \n", size);
 
     read_buffer.insert(read_buffer.end(), buf, buf + size);
+    connection.parse_cell(read_buffer, send_buffer, initiator_log);
 
-    connection.parse_cell(read_buffer);
+    if (!send_buffer.empty()) {
+
+      printf("writing! %zu \n", send_buffer.size());
+      mbedtls_ssl_write(&ssl, send_buffer.data(), send_buffer.size());
+
+      fwrite(send_buffer.data(), send_buffer.size(), 1, from_me);
+      fflush(from_me);
+      send_buffer.clear();
+    }
 
     fwrite(buf, size, 1, log_file);
     fflush(log_file);
--- src/tor.hpp
+++ src/tor.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include "mbedtls/pk.h"
+#include "mbedtls/sha256.h"
+#include "mbedtls/x509.h"
+#include "mbedtls/x509_crl.h"
+#include "mbedtls/x509_crt.h"
 #include <arpa/inet.h>
 #include <cstddef>
 #include <cstdint>
+#include <cstdlib>
+#include <ctime>
 #include <linux/tls.h>
 #include <mbedtls/aes.h>
 #include <mbedtls/ctr_drbg.h>
@@ -15,6 +21,8 @@
 #include <netinet/tcp.h>
 #include <optional>
 #include <print>
+#include <sodium.h>
+#include <sodium/crypto_sign.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,9 +36,43 @@
 #define CELL_BODY_LEN 509
 
 class TorConnection {
+
 public:
-  TorConnection() {}
-  ~TorConnection() {}
+  TorConnection(std::vector<uint8_t> my_cert,
+                std::vector<uint8_t> local_KP_relayid_ed,
+                std::vector<uint8_t> link_secret_key,
+                std::vector<uint8_t> link_public_key, uint32_t my_addr,
+                uint32_t other_addr, std::vector<uint8_t> rsa_secret_key)
+      : local_KP_relayid_ed(local_KP_relayid_ed),
+        link_secret_key(link_secret_key), link_public_key(link_public_key),
+        my_addr(htonl(my_addr)), other_addr(htonl(other_addr)) {
+    local_certs.push_back({0x06, my_cert});
+
+    mbedtls_pk_init(&rsa_pk);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
+    mbedtls_entropy_init(&entropy);
+
+    mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                          (const unsigned char *)"client", 6);
+
+    mbedtls_pk_parse_key(&rsa_pk, rsa_secret_key.data(), rsa_secret_key.size(),
+                         NULL, 0, mbedtls_ctr_drbg_random, &ctr_drbg);
+
+    uint8_t der_buf[4096];
+    int der_len =
+        mbedtls_pk_write_pubkey_der(&rsa_pk, der_buf, sizeof(der_buf));
+
+    printf("%s\n", der_buf);
+
+    local_KP_relayid_rsa.insert(local_KP_relayid_rsa.end(),
+                                (uint8_t *)der_buf + (4096 - der_len),
+                                der_buf + 4096);
+  }
+  ~TorConnection() {
+    mbedtls_entropy_free(&entropy);
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_pk_free(&rsa_pk);
+  }
 
   void generate_versions_cell(std::vector<uint8_t> &return_buffer) {
     std::vector<uint8_t> data = {};
@@ -40,7 +82,11 @@ public:
     generate_cell_variable(return_buffer, 0, 7, data);
   }
 
-  void parse_cell(std::vector<uint8_t> &return_buffer) {
+  bool sent_auth = false;
+  std::vector<uint8_t> responder_log;
+  void parse_cell(std::vector<uint8_t> &return_buffer,
+                  std::vector<uint8_t> &send_buffer,
+                  std::vector<uint8_t> &initiator_log) {
     uint64_t cursor = 0;
     auto circ_id = parse_uint16(return_buffer, cursor);
     if (!circ_id.has_value()) {
@@ -68,10 +114,6 @@ public:
       switch (command.value()) {
       case 7: {
         // cool we dont really gaf its their job to close the connection
-        break;
-      }
-      case 8: {
-        // we also dont gaf about the incoming netinfo
         break;
       }
       case 129: {
@@ -103,11 +145,44 @@ public:
       if (!payload.has_value()) {
         return;
       }
+
+      switch (command.value()) {
+      case 8: {
+        // we also dont gaf about the incoming netinfo
+        // but we should get ready to send out own packets!
+
+        if (!sent_auth) {
+
+          printf("sending my auth!\n");
+          generate_cert_cell(send_buffer);
+          initiator_log.insert(initiator_log.end(), send_buffer.begin(),
+                               send_buffer.end());
+
+          generate_authenticate_cell(send_buffer, initiator_log);
+          generate_netinfo_cell(send_buffer);
+        }
+
+        break;
+      }
+      }
     }
 
     printf("%i\n", ntohs(circ_id.value()));
     printf("%i\n", command.value());
     return_buffer.erase(return_buffer.begin(), return_buffer.begin() + cursor);
+    if (!sent_auth) {
+
+      responder_log.insert(responder_log.end(), return_buffer.begin(),
+                           return_buffer.begin() + cursor);
+
+      initiator_log.insert(initiator_log.end(), send_buffer.begin(),
+                           send_buffer.end());
+    }
+
+    // we need it for the log but then we can stop logging
+    if (command.value() == 8) {
+      sent_auth = true;
+    }
   }
 
 private:
@@ -192,7 +267,38 @@ private:
     HS_IP_CC_SIGNING = 0x0B,
   };
 
-  std::vector<std::pair<uint8_t, std::vector<uint8_t>>> certs;
+  std::vector<uint8_t> remote_KP_relayid_rsa;
+  std::vector<uint8_t> local_KP_relayid_rsa;
+  std::vector<uint8_t> local_KP_relayid_ed;
+  std::vector<uint8_t> remote_KP_relayid_ed;
+  void parse_x509_cert(std::vector<uint8_t> &cert_rsa_buffer) {
+    mbedtls_x509_crt crt;
+    mbedtls_x509_crt_init(&crt);
+    mbedtls_x509_crt_parse(&crt, cert_rsa_buffer.data(),
+                           cert_rsa_buffer.size());
+    uint8_t der_buf[4096];
+    // tf do we write to the end???
+    int der_len =
+        mbedtls_pk_write_pubkey_der(&crt.pk, der_buf, sizeof(der_buf));
+    mbedtls_x509_crt_free(&crt);
+
+    FILE *rsa_file = fopen("rsa.log", "w");
+    printf("%s\n", der_buf);
+
+    fwrite(der_buf, strlen((char *)der_buf), 1, rsa_file);
+    remote_KP_relayid_rsa.insert(remote_KP_relayid_rsa.end(),
+                                 der_buf + 4096 - der_len, der_buf + 4096);
+    fclose(rsa_file);
+  }
+
+  void parse_rsa_cert(std::vector<uint8_t> &cert_rsa_buffer) {
+    remote_KP_relayid_ed.insert(remote_KP_relayid_ed.end(),
+                                cert_rsa_buffer.begin(),
+                                cert_rsa_buffer.begin() + 32);
+  }
+
+  std::vector<std::pair<uint8_t, std::vector<uint8_t>>> remote_certs;
+  std::vector<std::pair<uint8_t, std::vector<uint8_t>>> local_certs;
   std::optional<bool> parse_cert(std::vector<uint8_t> &cert_buffer,
                                  uint64_t &cursor) {
     auto count = parse_uint8(cert_buffer, cursor);
@@ -213,13 +319,15 @@ private:
       if (!cert.has_value())
         return {};
 
-      certs.push_back({cert_type.value(), cert.value()});
+      remote_certs.push_back({cert_type.value(), cert.value()});
 
       switch ((CertType)cert_type.value()) {
       case TLS_LINK_X509:
         printf("TLS_LINK_X509 = 0x01\n");
         break;
       case RSA_ID_X509:
+        parse_x509_cert(cert.value());
+
         printf("RSA_ID_X509 = 0x02\n");
         break;
       case LINK_AUTH_X509:
@@ -292,4 +400,127 @@ private:
 
     return true;
   }
+  mbedtls_pk_context rsa_pk;
+  std::vector<uint8_t> link_secret_key;
+  std::vector<uint8_t> link_public_key;
+  void generate_authenticate_cell(std::vector<uint8_t> &send_buffer,
+                                  std::vector<uint8_t> &initiator_log) {
+    for (auto challenge : auth_challenges) {
+      std::vector<uint8_t> data = {};
+      uint16_t auth_type = 0x03;
+      auth_type = htons(auth_type);
+
+      std::vector<uint8_t> auth_buffer;
+      std::string auth_type_str = "AUTH0003";
+      auth_buffer.insert(auth_buffer.end(), auth_type_str.begin(),
+                         auth_type_str.end());
+
+      // rsa
+
+      uint8_t local_hash_rsa[32];
+      mbedtls_sha256(local_KP_relayid_rsa.data(), local_KP_relayid_rsa.size(),
+                     local_hash_rsa, false);
+
+      auth_buffer.insert(auth_buffer.end(), local_hash_rsa,
+                         local_hash_rsa + 32);
+
+      uint8_t remote_hash_rsa[32];
+      mbedtls_sha256(remote_KP_relayid_rsa.data(), remote_KP_relayid_rsa.size(),
+                     remote_hash_rsa, false);
+
+      auth_buffer.insert(auth_buffer.end(), remote_hash_rsa,
+                         remote_hash_rsa + 32);
+
+      // ed time!
+      auth_buffer.insert(auth_buffer.end(), local_KP_relayid_ed.begin(),
+                         local_KP_relayid_ed.end());
+
+      auth_buffer.insert(auth_buffer.end(), remote_KP_relayid_ed.begin(),
+                         remote_KP_relayid_ed.end());
+
+      // slog
+      uint8_t remote_log_hash[32];
+      mbedtls_sha256(responder_log.data(), responder_log.size(),
+                     remote_log_hash, false);
+      auth_buffer.insert(auth_buffer.end(), remote_log_hash,
+                         remote_log_hash + 32);
+      // clog
+      uint8_t local_log_hash[32];
+      mbedtls_sha256(initiator_log.data(), initiator_log.size(), local_log_hash,
+                     false);
+      auth_buffer.insert(auth_buffer.end(), local_log_hash,
+                         local_log_hash + 32);
+
+      // auth_buffer.insert()
+      // data.insert(data.end(), (uint8_t *)&auth_type,
+      //             (uint8_t *)&auth_type + sizeof(uint16_t));
+      //
+      // unsigned char signature[crypto_sign_BYTES];
+      // crypto_sign_detached(signature, NULL, challenge.data(),
+      // challenge.size(),
+      //                      link_secret_key.data());
+      //
+      // uint16_t auth_len = crypto_sign_BYTES;
+      // auth_len = htons(auth_len);
+      // data.insert(data.end(), (uint8_t *)&auth_len,
+      //             (uint8_t *)&auth_len + sizeof(uint16_t));
+      //
+      // data.insert(data.end(), signature, signature +
+      // crypto_sign_BYTES);
+      generate_cell_variable(send_buffer, 0, 131, data);
+    }
+
+    auth_challenges.clear();
+  }
+
+  void generate_cert_cell(std::vector<uint8_t> &send_buffer) {
+    std::vector<uint8_t> data = {};
+    data.push_back(1); // one cert!
+
+    for (auto cert : local_certs) {
+      // first cert
+      data.push_back(cert.first); // one cert!
+
+      uint16_t cert_len = cert.second.size();
+      cert_len = htons(cert_len);
+
+      data.insert(data.end(), (uint8_t *)&cert_len,
+                  (uint8_t *)&cert_len + sizeof(uint16_t));
+
+      data.insert(data.end(), cert.second.begin(), cert.second.end());
+    }
+
+    generate_cell_variable(send_buffer, 0, 129, data);
+  }
+
+  uint32_t my_addr, other_addr;
+  // https://torspec-12e191.pages.torproject.net/tor-spec/negotiating-channels.html#NETINFO-cells
+  void generate_netinfo_cell(std::vector<uint8_t> &send_buffer) {
+    uint32_t epoch = (uint32_t)time(NULL);
+    epoch = htonl(epoch);
+
+    std::vector<uint8_t> data = {};
+    data.insert(data.end(), (uint8_t *)&epoch,
+                (uint8_t *)&epoch + sizeof(uint32_t));
+
+    // other addr
+    data.push_back(0x04); // ipv4
+    data.push_back(sizeof(uint32_t));
+    data.insert(data.end(), (uint8_t *)&other_addr,
+                (uint8_t *)&other_addr + sizeof(uint32_t));
+
+    // my addrs
+    data.push_back(1); // i have one address
+
+    data.push_back(0x04); // ipv4
+    data.push_back(sizeof(uint32_t));
+    data.insert(data.end(), (uint8_t *)&my_addr,
+                (uint8_t *)&my_addr + sizeof(uint32_t));
+
+    generate_cell_fixed(send_buffer, 0, 8, data);
+  }
+
+  // crypto bs
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_entropy_context entropy;
 };
